62ac0ce1e89830026552f8d7a17a758fe4ec8728
Dr Matt James <mattkjames7@gmail.com>
2018-11-22 17:11:41 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="TimeTools",
-    version="0.0.1",
+    version="0.0.3",
     author="Matthew Knight James",
     author_email="mattkjames7@gmail.com",
     description="A package containing some simple tools to manage dates and times.",
@@ -20,7 +20,6 @@ setuptools.setup(
     ],
     install_requires=[
 		'numpy',
-		'datetime'
-		'scipy'
+		'scipy',
 	],
 )

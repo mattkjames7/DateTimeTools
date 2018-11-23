4efa3c6cf03e5c042356b0bed17ad472c841ea03
mattkjames7 <mattkjames7@gmail.com>
2018-11-23 11:59:45 +0000

    changed module name in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="TimeTools",
+    name="DateTimeTools",
     version="0.0.3",
     author="Matthew Knight James",
     author_email="mattkjames7@gmail.com",

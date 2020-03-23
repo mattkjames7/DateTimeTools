b3f869d3a14dc070f69b25488139831a4a155879
mattkjames7 <mattkjames7@gmail.com>
2020-03-23 09:47:25 +0000

    updated version number, changed DayNo to accept arrays

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="DateTimeTools",
-    version="0.0.4",
+    version="0.0.5",
     author="Matthew Knight James",
     author_email="mattkjames7@gmail.com",
     description="A package containing some simple tools to manage dates and times.",
@@ -21,7 +21,6 @@ setuptools.setup(
     install_requires=[
 		'numpy',
 		'scipy',
-		'DateTimeTools',
 		'kpindex',
 		'pyomnidata',
 	],

398fbd4bb55383918b984c3b49094f3a3419feba
mattkjames7 <mattkjames7@gmail.com>
2020-09-28 23:41:12 +0100

    fixed some annoying bugs, updated version again

--- DateTimeTools/CDFEpochtoDate.py
+++ DateTimeTools/CDFEpochtoDate.py
@@ -6,8 +6,7 @@ from .DateJoin import DateJoin
 
 def CDFEpochtoDate(cdfe):
 	'''
-	Computes the date and time from the CDF Epoch (milliseconds since
-	Jan 01 0000).
+	Computes the date and time from the CDF Epoch.
 	
 	Inputs
 	======
@@ -25,10 +24,16 @@ def CDFEpochtoDate(cdfe):
 	'''
 
 	#convert using cdflib
-	dt = cdflib.cdfepoch.breakdown_epoch(cdfe).T
+	dt = cdflib.cdfepoch.breakdown(cdfe,to_np=True).T
 	
+	#check if there is more time information than just ms
+	if dt.shape[0] == 9:
+		ms = dt[6] + dt[7]/1000.0 + dt[8]/1000000.0
+	else:
+		ms = dt[6]
+		
 	#extract dates and times
 	Date = DateJoin(dt[0],dt[1],dt[2])
-	ut = HHMMtoDec(dt[3],dt[4],dt[5],dt[6])
+	ut = HHMMtoDec(dt[3],dt[4],dt[5],ms)
 	
 	return Date,ut
--- build/lib/DateTimeTools/HHMMtoDec.py
+++ build/lib/DateTimeTools/HHMMtoDec.py
@@ -27,20 +27,20 @@ def HHMMtoDec(hh,mm=None,ss=None,ms=None):
 
 	#convert the inputs into the exact dtypes required for C++
 	_n = _CTConv(np.size(hh),'c_int')
-	_hh = _CTConv(hh,'c_int_ptr')
+	_hh = _CTConv(hh,'c_double_ptr')
 	if not mm is None:
-		_mm = _CTConv(mm,'c_int_ptr')
+		_mm = _CTConv(mm,'c_double_ptr')
 	else:
-		_mm = np.zeros(_n,dtype='int32')
+		_mm = np.zeros(_n,dtype='flaot64')
 	if not ss is None:
-		_ss = _CTConv(ss,'c_int_ptr')
+		_ss = _CTConv(ss,'c_double_ptr')
 	else:
-		_ss = np.zeros(_n,dtype='int32')
+		_ss = np.zeros(_n,dtype='float64')
 	if not ms is None:
-		_ms = _CTConv(ms,'c_int_ptr')
+		_ms = _CTConv(ms,'c_double_ptr')
 	else:
-		_ms = np.zeros(_n,dtype='int32')
-	_ut = np.zeros(_n,dtype='float32')
+		_ms = np.zeros(_n,dtype='float64')
+	_ut = np.zeros(_n,dtype='float64')
 
 	#call the C++ function
 	_CHHMMtoDec(_n,_hh,_mm,_ss,_ms,_ut)

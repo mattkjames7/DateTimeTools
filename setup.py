1366ee4fda3256f19e01e19cb5847b25d5e73d0d
mattkjames7 <mattkjames7@gmail.com>
2021-02-01 11:39:01 +0000

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="DateTimeTools",
-    version="0.2.0",
+    version="0.2.1",
     author="Matthew Knight James",
     author_email="mattkjames7@gmail.com",
     description="A package containing some simple tools to manage dates and times.",

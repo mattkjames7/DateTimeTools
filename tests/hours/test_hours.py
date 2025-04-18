10685b1ffcffa651bda8c70b079c581a7f1b3531
matt james <mattkjames7@gmail.com>
2025-04-18 15:25:18 +0100

    more tests

---
+++ tests/hours/test_hours.py
@@ -0,0 +1,24 @@
+import DateTimeTools as dt
+
+
+def test_hourstohhmm():
+    """
+    test converting hours to hhmm
+    """
+
+    hh, mm, ss, ms = dt.DectoHHMM(22.25)
+
+    assert hh == 22
+    assert mm == 15
+    assert ss == 0
+    assert ms == 0.0
+
+
+def test_hhmmtohours():
+    """
+    reverse of the above
+    """
+
+    ut = dt.DectoHHMM(22, 15, 0, 0)
+
+    assert ut == 22.25

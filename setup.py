a35df6b70103b59487cce844aecd47de66cc33fc
matt <mattkjames7@gmail.com>
2025-12-26 00:44:34 +0000

    udpate build commands

--- setup.py
+++ setup.py
@@ -23,40 +23,12 @@ class CustomBuild(build_py):
 				#cmd = ['make', '-C', 'DateTimeTools/__data/datetime']
 				cwd = os.getcwd()
 				os.chdir('DateTimeTools/__data/datetime/')
-				cmd = ["cmake","-DCMAKE_INSTALL_PREFIX=/usr/local","-B","build"]
+				cmd = ["cmake","-DCMAKE_INSTALL_PREFIX=$(pwd)","-B","build"]
 				subprocess.check_call(cmd, stderr=subprocess.STDOUT)
 				cmd = ["cmake","--build","build"]
 				subprocess.check_call(cmd, stderr=subprocess.STDOUT)
-				
-				# Copy the built library to the lib directory where Python expects it
-				import shutil
-				lib_dir = os.path.join(os.getcwd(), 'lib')
-				os.makedirs(lib_dir, exist_ok=True)
-				
-				# Determine library extension based on platform
-				if platform.system() == 'Linux':
-					lib_ext = 'so'
-				elif platform.system() == 'Darwin':
-					lib_ext = 'dylib'
-				else:
-					lib_ext = 'so'
-				
-				# Find and copy the library from the build directory
-				lib_name = f'libdatetime.{lib_ext}'
-				src_lib = os.path.join('build', lib_name)
-				if os.path.isfile(src_lib):
-					dst_lib = os.path.join(lib_dir, lib_name)
-					shutil.copy2(src_lib, dst_lib)
-					print(f"Copied {src_lib} to {dst_lib}")
-				else:
-					# Try to find in src subdirectory
-					src_lib = os.path.join('build', 'src', lib_name)
-					if os.path.isfile(src_lib):
-						dst_lib = os.path.join(lib_dir, lib_name)
-						shutil.copy2(src_lib, dst_lib)
-						print(f"Copied {src_lib} to {dst_lib}")
-					else:
-						print(f"Warning: Could not find {lib_name} in build directory")
+				cmd = ["cmake","--install","build"]
+				subprocess.check_call(cmd, stderr=subprocess.STDOUT)
 				
 				os.chdir(cwd)
 		except subprocess.CalledProcessError as e:
@@ -102,7 +74,7 @@ setup(
 	long_description_content_type="text/markdown",
 	url="https://github.com/mattkjames7/DateTimeTools",
 	packages=find_packages(),
-	package_data={'DateTimeTools': ['__data/datetime/**/*']},
+	package_data={'datetime': ['**/*']},
 	cmdclass={'build_py': CustomBuild},  
 	classifiers=[
 		"Programming Language :: Python :: 3",

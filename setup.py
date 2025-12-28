968cc9d04542ae9a13aef7ef24e79343c461a5f0
Matt James <mattkjames7@gmail.com>
2025-12-28 15:54:47 +0000

    update setup

--- setup.py
+++ setup.py
@@ -89,11 +89,15 @@ def getversion():
 	
 version = getversion()
 
-ext_modules = [
-    Extension("DateTimeTools._wheelstub", ["DateTimeTools/_wheelstub.c"],
-  	extra_compile_args=["-O2", "-fno-lto"],
-  	extra_link_args=["-Wl,--no-as-needed"],)
-]
+if platform.system() == "Linux":
+	ext_modules = [
+		Extension("DateTimeTools._wheelstub", ["DateTimeTools/_wheelstub.c"],
+		extra_compile_args=["-O2", "-fno-lto"],
+		extra_link_args=["-Wl,--no-as-needed"],)
+	]
+else:
+	ext_modules = [Extension("DateTimeTools._wheelstub", ["DateTimeTools/_wheelstub.c"])]
+
 
 setup(
 	name="DateTimeTools",
